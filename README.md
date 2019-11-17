d2b5d34dad39e41a7b61076c85f58773d6337f24
Tang jiyi <37148354+Tangjiyi0416@users.noreply.github.com>
2019-11-17 11:45:36 +0800

    added a config file and redone the description

--- README.md
+++ README.md
@@ -4,7 +4,24 @@ Or a wonderful always-cast wand that is infact useless because of its more than 
 
 Hopefully, it will no longer be the case with this mod.
 
-This mod adds some upgrade spells for wands includiing "Reduce Recharge Time", "Anti-shuffle" and more!
+This mod adds many enchant spells for wands!
+
+Currently have enchantments:(You can tweek the number in config file.)
+  
+-Wand Anti Shuffle:Turn your shuffle wand into a not shuffle wand.
+-Reduce Wand Cast Delay:Reduce the cast delay of your wand.
+-Reduce Wand Recharge Time:Reduce the recharge time of your wand.
+-Increase Wand Maximum Mana:Increase the maximum mana of your wand.
+-Increase Wand Mana Charge Speed:Increase the mana charge speed of your wand.
+-Increase Wand Capacity:Increase the capacity of your wand.
+-Increase Wand Spells Per Cast:Increase the spell/cast of your wand.
+-Reduces Wand Sprea Degrees:Reduce the spread of your wand.
+
+How to use :
+  1.get a wand and any enchantment
+  2.put the enchantment into the wand
+  3.cast it like any other spell
+  4.profit
 
 Hope you enjoy this mod. And if you have any suggestions be sure to leave me a comment!
 
--- files/Enchantments/actions.lua
+++ files/Enchantments/actions.lua
@@ -1,8 +1,9 @@
-dofile("mods/Wandcraft/lib/wand_edit_utilities.lua")
+dofile("lib/wand_edit_utilities.lua")
+dofile("config.lua")
 table.insert( actions,{
 	id          = "WAND_ENCHANT_SHUFFLE_DECK_WHEN_EMPTY",
 	name 		= "Wand Anti Shuffle",
-	description = "It turns shuffle to no",
+	description = "Turn your shuffle wand into a not shuffle wand",
 	sprite 		= "files/Enchantments/anti_shuffle.png",
 	type 		= ACTION_TYPE_OTHER,
 	spawn_level                       = "0,4,5,6", 
@@ -35,7 +36,7 @@ table.insert( actions,{
 		local abi_com = GetCurrentWandAbilityComponent()
 		if abi_com~=nil then  --credit kermit tears for solving the first time loading problem
 			local fire_rate_wait = ComponentObjectGetValue(abi_com, "gunaction_config", "fire_rate_wait")
-			ComponentObjectSetValue(abi_com , "gunaction_config", "fire_rate_wait", tostring((tonumber(fire_rate_wait)-10)))
+			ComponentObjectSetValue(abi_com , "gunaction_config", "fire_rate_wait", tostring((tonumber(fire_rate_wait)-WANDCRAFT_REDUCE_FIRE_RATE_WAIT)))
 			ACTION_DRAW_RELOAD_TIME_INCREASE = fire_rate_wait-10
 			RemoveCard(c.action_id)
 			c.fire_rate_wait = c.fire_rate_wait + 10
@@ -56,7 +57,7 @@ table.insert( actions,{
 		local abi_com = GetCurrentWandAbilityComponent()
 		if abi_com~=nil then  --credit kermit tears for solving the first time loading problem
 			local reload_time = ComponentObjectGetValue(abi_com, "gun_config", "reload_time")
-			ComponentObjectSetValue(abi_com , "gun_config", "reload_time", tostring((tonumber(reload_time)-10)))
+			ComponentObjectSetValue(abi_com , "gun_config", "reload_time", tostring((tonumber(reload_time)-WANDCRAFT_REDUCE_RELOAD_TIME)))
 			current_reload_time = reload_time-10
 			RemoveCard(c.action_id)
 			c.fire_rate_wait = c.fire_rate_wait + 10
@@ -77,7 +78,7 @@ table.insert( actions,{
 		local abi_com = GetCurrentWandAbilityComponent()
 		if abi_com~=nil then  --credit kermit tears for solving the first time loading problem
 			local mana_max = ComponentGetValue(abi_com, "mana_max")
-			ComponentSetValue(abi_com , "mana_max", tostring((tonumber(mana_max)+150)))
+			ComponentSetValue(abi_com , "mana_max", tostring((tonumber(mana_max)+WANDCRAFT_INCREASE_MANA_MAX)))
 			mana = mana_max+150
 			RemoveCard(c.action_id)
 			c.fire_rate_wait = c.fire_rate_wait + 10
@@ -98,7 +99,7 @@ table.insert( actions,{
 		local abi_com = GetCurrentWandAbilityComponent()
 		if abi_com~=nil then  --credit kermit tears for solving the first time loading problem
 			local mana_charge_speed = ComponentGetValue(abi_com, "mana_charge_speed")
-			ComponentSetValue(abi_com , "mana_charge_speed", tostring((tonumber(mana_charge_speed)+35)))
+			ComponentSetValue(abi_com , "mana_charge_speed", tostring((tonumber(mana_charge_speed)+WANDCRAFT_INCREASE_MANA_CHARGE_SPEED)))
 			RemoveCard(c.action_id)
 			c.fire_rate_wait = c.fire_rate_wait + 10
 		end
@@ -106,8 +107,8 @@ table.insert( actions,{
 })
 table.insert( actions,{
 	id          = "WAND_ENCHANT_DECK_CAPACITY",
-	name 		= "Increase Wand Deck Capacity",
-	description = "It increases deck capacity",
+	name 		= "Increase Wand Capacity",
+	description = "It increases capacity",
 	sprite 		= "files/Enchantments/increase_deck_capacity.png",
 	type 		= ACTION_TYPE_OTHER,
 	spawn_level                       = "0,4,5,6", 
@@ -118,15 +119,15 @@ table.insert( actions,{
 		local abi_com = GetCurrentWandAbilityComponent()
 		if abi_com~=nil then  --credit kermit tears for solving the first time loading problem
 			local deck_capacity = ComponentObjectGetValue(abi_com , "gun_config", "deck_capacity")
-			ComponentObjectSetValue(abi_com , "gun_config", "deck_capacity", tostring((tonumber(deck_capacity)+1)))
+			ComponentObjectSetValue(abi_com , "gun_config", "deck_capacity", tostring((tonumber(deck_capacity)+WANDCRAFT_INCREASE_DECK_CAPACITY)))
 			RemoveCard(c.action_id)
 		end
 	end,
 })
 table.insert( actions,{
 	id          = "WAND_ENCHANT_ACTIONS_PER_ROUND",
-	name 		= "Increase Wand Actions Per Round",
-	description = "It increases actions per round",
+	name 		= "Increase Wand Spells Per Cast",
+	description = "It increases spells per sast",
 	sprite 		= "files/Enchantments/increase_actions_per_round.png",
 	type 		= ACTION_TYPE_OTHER,
 	spawn_level                       = "0,4,5,6", 
@@ -137,7 +138,7 @@ table.insert( actions,{
 		local abi_com = GetCurrentWandAbilityComponent()
 		if abi_com~=nil then  --credit kermit tears for solving the first time loading problem
 			local actions_per_round = ComponentObjectGetValue(abi_com , "gun_config", "actions_per_round")
-			ComponentObjectSetValue(abi_com , "gun_config", "actions_per_round", tostring((tonumber(actions_per_round)+1)))
+			ComponentObjectSetValue(abi_com , "gun_config", "actions_per_round", tostring((tonumber(actions_per_round)+WANDCRAFT_INCREASE_ACTIONS_PER_ROUND)))
 			gun.actions_per_round = actions_per_round
 			RemoveCard(c.action_id)
 			c.fire_rate_wait = c.fire_rate_wait + 10
@@ -158,7 +159,7 @@ table.insert( actions,{
 		local abi_com = GetCurrentWandAbilityComponent()
 		if abi_com~=nil then  --credit kermit tears for solving the first time loading problem
 			local spread_degrees = ComponentObjectGetValue(abi_com , "gunaction_config", "spread_degrees")
-			ComponentObjectSetValue(abi_com , "gunaction_config", "spread_degrees", tostring((tonumber(spread_degrees)-1)))
+			ComponentObjectSetValue(abi_com , "gunaction_config", "spread_degrees", tostring((tonumber(spread_degrees)-WANDCRAFT_REDUCE_SPREAD_DEGREES)))
 			RemoveCard(c.action_id)
 			c.fire_rate_wait = c.fire_rate_wait + 10
 		end
